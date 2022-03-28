953be841b8008fea587068388c2bb875c03e1698
Moyo1a <88391483+Moyo1a@users.noreply.github.com>
2022-03-27 18:20:40 -0700

    first one

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostLists from './components/PostLists';
+
+function App() {
+  return (
+    <div className="App">
+    <PostLists />
+    </div>
+  );
+}
+
+export default App;
